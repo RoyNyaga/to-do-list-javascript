d711062052eb746ba50fde6316f0b6523c2bbe2e
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-25 17:51:36 +0100

    add all files

--- dist/main.js
+++ dist/main.js
@@ -184,63 +184,63 @@ const Forms = (() => {
 
 const Mixing = (() => {
   const openToDoForm = () => {
-    const todoFormDiv = document.querySelector('#todo-form-div')
-    todoFormDiv.innerHTML = src_Forms.todo
-  }
+    const todoFormDiv = document.querySelector('#todo-form-div');
+    todoFormDiv.innerHTML = src_Forms.todo;
+  };
 
   const closeTodoForm = () => {
-    const todoFormDiv = document.querySelector('#todo-form-div')
-    todoFormDiv.innerHTML = ''
-  }
+    const todoFormDiv = document.querySelector('#todo-form-div');
+    todoFormDiv.innerHTML = '';
+  };
 
   const openProjectForm = () => {
-    const header = document.querySelector('#headerDiv')
-    header.innerHTML = src_Forms.project
-  }
+    const header = document.querySelector('#headerDiv');
+    header.innerHTML = src_Forms.project;
+  };
 
   const closeProjectForm = () => {
-    const header = document.querySelector('#headerDiv')
-    header.innerHTML = ''
-  }
+    const header = document.querySelector('#headerDiv');
+    header.innerHTML = '';
+  };
 
   const todoValidationMessage = () => {
-    const validationDiv = document.querySelector('#validation-info')
-    validationDiv.innerHTML = 'title and description must be greater than 6 letters'
-  }
+    const validationDiv = document.querySelector('#validation-info');
+    validationDiv.innerHTML = 'title and description must be greater than 6 letters';
+  };
 
   const todoValidationExit = () => {
-    const validationDiv = document.querySelector('#validation-info')
-    validationDiv.innerHTML = ''
-  }
+    const validationDiv = document.querySelector('#validation-info');
+    validationDiv.innerHTML = '';
+  };
 
   const projectValidationMessage = () => {
-    const validationDiv = document.querySelector('#validation-info')
-    validationDiv.innerHTML = 'Project already exist'
-  }
+    const validationDiv = document.querySelector('#validation-info');
+    validationDiv.innerHTML = 'Project already exist';
+  };
   const projectValidationExit = () => {
-    const validationDiv = document.querySelector('#validation-info')
-    validationDiv.innerHTML = ''
-  }
+    const validationDiv = document.querySelector('#validation-info');
+    validationDiv.innerHTML = '';
+  };
 
   const editTodoValidation = () => {
-    const validationDiv = document.querySelector('#validation-info')
-    validationDiv.innerHTML = 'to do must be greater than 5 letters'
-  }
+    const validationDiv = document.querySelector('#validation-info');
+    validationDiv.innerHTML = 'to do must be greater than 5 letters';
+  };
 
   const editTodoExitValidation = () => {
-    const validationDiv = document.querySelector('#validation-info')
-    validationDiv.innerHTML = ''
-  }
+    const validationDiv = document.querySelector('#validation-info');
+    validationDiv.innerHTML = '';
+  };
 
   const deletProjectValidation = () => {
-    const validationDiv = document.querySelector('#validation-info')
-    validationDiv.innerHTML = 'Cannot delete default project'
-  }
+    const validationDiv = document.querySelector('#validation-info');
+    validationDiv.innerHTML = 'Cannot delete default project';
+  };
 
   const deletProjectExitValidation = () => {
-    const validationDiv = document.querySelector('#validation-info')
-    validationDiv.innerHTML = ''
-  }
+    const validationDiv = document.querySelector('#validation-info');
+    validationDiv.innerHTML = '';
+  };
 
   return {
     openToDoForm,
@@ -254,9 +254,9 @@ const Mixing = (() => {
     editTodoValidation,
     editTodoExitValidation,
     deletProjectValidation,
-    deletProjectExitValidation
-  }
-})()
+    deletProjectExitValidation,
+  };
+})();
 
 /* harmony default export */ var src_Mixing = (Mixing);
 
